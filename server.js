db31d519bfd1a034d122103ddca779c730b55153
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-16 01:30:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
-const port  =5000;
+const port = 5000;
 const app = express();
 const db = require("./models");
 app.use(express.json());
-db.sequelize.sync().then(()=>{
-    app.listen(port, ()=>{
-        console.log("server is running at port 5000");
-    })
-}).catch((err)=>{
+
+const userRoutes = require("./routes/user_routes");
+app.use("/api/users", userRoutes);
+
+const profileRoutes = require("./routes/profile_routes");
+app.use("/api/profiles", profileRoutes);
+
+const postRoutes = require("./routes/post_routes");
+app.use("/api/posts", postRoutes);
+db.sequelize
+  .sync({force:false})
+  .then(() => {
+    app.listen(port, () => {
+      console.log("server is running at port 5000");
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-
-console.log("Allah HO Akbar")
+console.log("Allah HO Akbar");
