34fe46f014a133677cc6e3ba3c230adf9212851d
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-16 01:30:07 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,19 +1,19 @@
-module.exports = (sequelize,DataTypes) =>{
-    const User = sequelize.define("User",{
-    username:{
-    type:DataTypes.STRING,
-    allowNull:false,
-    }
- });
- User.associate = models =>{
-    User.hasMany(models.Post,{
-        onDelete: "cascade",
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  User.associate = (models) => {
+    User.hasOne(models.Profile, {
+      onDelete: "cascade",
     });
-    User.associate = models =>{
-        User.hasOne(models.Profile,{
-            onDelete:"cascade",
-        })
-    }
- }
- return User;
-}
+  };
+  User.associate = (models) => {
+    User.hasMany(models.Post, {
+      onDelete: "cascade",
+    });
+  };
+  return User;
+};
