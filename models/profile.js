824b4d17411b5d2482d93a89e5b47f50714223fa
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-16 01:30:02 +0500

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,16 +1,16 @@
-module.exports = (sequelize, DataTypes)=>{
-    const Profile = sequelize.define("Profile", {
-        name:{
-            type:DataTypes.STRING,
-            allowNull:false,
-        },
+module.exports = (sequelize, DataTypes) => {
+  const Profile = sequelize.define("Profile", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  Profile.associate = (models) => {
+    Profile.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
     });
-    Profile.associate = models =>{
-        Profile.belongsTo(models.User,{
-            foreignKey:{
-                allowNull:false,
-            },
-        });
-    };
-    return Profile;
+  };
+  return Profile;
 };
